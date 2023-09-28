b5e5f93e3e46b9606557e09355e7051f19094cd5
Florian Schwaiger <f.schwaiger@tum.de>
2023-09-28 23:17:05 +0200

    fix: number of elements returned from indexing iterators

--- code/+containers/@Grid/numArgumentsFromSubscript.m
+++ code/+containers/@Grid/numArgumentsFromSubscript.m
@@ -1,6 +1,7 @@
 function n = numArgumentsFromSubscript(self, s, indexingContext)
     % Returns 1, to make brace {} indexing work
     
+    % we are accessing a struct field or property from the data matrix
     if s(1).type == "{}" && numel(s) > 1 && s(2).type == "."
         args = subs2args(self, s(1).subs);
         n = numel(self.Data(args{:}));
@@ -15,21 +16,26 @@ function n = numArgumentsFromSubscript(self, s, indexingContext)
     % cannot execute logic down below
     if isempty(s)
         n = 1;
-        return
-    end
+        
+    % slicing the grid never changes the dimensionality of the output
+    elseif isequal(s(1), substruct('.', 'Iter')) ...
+            && numel(s) > 1 ...
+            && s(2).type == "()"
+        n = 1;
 
     % slicing iter by name cannot be handled by builtin code
-    if s(1).type == "." && s(1).subs == "Iter"
-        if numel(s) > 1 && s(2).type == "{}"
-            n = numel(s(2).subs);
-        else
-            n = 1;
-        end
-        return
-    end
-
+    elseif isequal(s(1), substruct('.', 'Iter')) ...
+            && numel(s) > 1 ...
+            && s(2).type == "{}" ...
+            && iscellstr(s(2).subs) ...
+            && not(ismember(':', s(2).subs))
+        n = numel(s(2).subs);
+        
     % everything else is normal
-    n = builtin('numArgumentsFromSubscript', self, s, indexingContext);
+    else
+        n = builtin('numArgumentsFromSubscript', self, s, indexingContext);
+        
+    end
 end
 
 %#release exclude file
